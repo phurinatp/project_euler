38f77332eb265cc892a52d71ea7f34955f06aac4
Phurinat Puekkham <phurinaix@gmail.com>
2022-05-15 18:38:50 +0700

    Update 8-Largest_product_in_a_series.js

--- 8-Largest_product_in_a_series.js
+++ 8-Largest_product_in_a_series.js
@@ -7,19 +7,46 @@ Find the thirteen adjacent digits in the 1000-digit number that have the greates
 
 let digits = `7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450`
 
-let max = -1/0;
-
 let adjacent = 13;
 
-for (let i = 0; i < (digits.length - adjacent + 1); i++) {
-    let product = 1;
-    for (let j = 0; j < adjacent; j++) {
-        product *= digits[i + j]
+// Brute Force
+function bestTotalProductUsingBruteForce(digits, k) {
+    let max = -1/0;
+    for (let i = 0; i < (digits.length - k + 1); i++) {
+        let product = 1;
+        for (let j = 0; j < k; j++) {
+            product *= digits[i + j]
+        }
+        if (product > max) {
+            max = product;
+        }
     }
-    if (product > max) {
-        max = product;
-    }
+    return max;
 }
 
-console.log(max); // expect 23514624000 (from 5576689664895)
+// Sliding Window
+function bestTotalProductUsingSlidingWindow(digits, k) {
+    let total = 1;
+    for (let i = 0; i < k; i++) {
+        total *= digits[i];
+    }
+    let maxTotal = total;
+    for (let i = 0; i < digits.length - k; i++) {
+        if (digits[i] == 0) {
+            let s = 1;
+            for (let j = i + 1; j < i + k; j++) {
+                s *= digits[j];
+            }
+            total = s;
+        } else {
+            total /= digits[i];
+        }
+        total *= digits[i + k];
+        maxTotal = Math.max(maxTotal, total);
+    }
+    return maxTotal;
+}
+
+console.log(bestTotalProductUsingBruteForce(digits, adjacent)); // expect 23514624000 (from 5576689664895)
+console.log(bestTotalProduct(digits, adjacent)); // expect 23514624000 (from 5576689664895)
 
